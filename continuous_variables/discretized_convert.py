49b2503d3b1302749e130d01c123b37de06d275b
btheodorou99 <99348934+btheodorou99@users.noreply.github.com>
2023-12-11 15:25:22 -0600

    Update discretized_convert.py

--- continuous_variables/discretized_convert.py
+++ continuous_variables/discretized_convert.py
@@ -21,17 +21,17 @@ for p in dataset:
   for v in p['visits']:
     new_labs = []
     new_values = []
-    for i in range(len(v[2])):
-      new_labs.append(idToLab[v[2][i]])
-      if isCategorical[idToLab[v[2][i]]]:
-        new_values.append(possibleValues[idToLab[v[2][i]]][v[3][i]])
+    for i in range(len(v[1])):
+      new_labs.append(idToLab[v[1][i]])
+      if isCategorical[idToLab[v[1][i]]]:
+        new_values.append(possibleValues[idToLab[v[1][i]]][v[2][i]])
       else:
-        new_values.append(formatCont(random.uniform(discretization[idToLab[v[2][i]]][v[3][i]], discretization[idToLab[v[2][i]]][v[3][i]+1]), idToLab[v[2][i]]))
+        new_values.append(formatCont(random.uniform(discretization[idToLab[v[1][i]]][v[2][i]], discretization[idToLab[v[1][i]]][v[2][i]+1]), idToLab[v[1][i]]))
     contType = 'Hours' if new_labs != [] else 'Age' if firstVisit else 'Days'
     if contType == 'Age':
       firstVisit = False
     new_cont = formatCont(random.uniform(discretization[contType][v[4][-1]], discretization[contType][v[4][-1]+1]), contType)
-    new_visits.append((v[1], new_labs, new_values, [new_cont]))
+    new_visits.append((v[0], new_labs, new_values, [new_cont]))
   p['visits'] = new_visits
 
 pickle.dump(dataset, open('results/datasets/haloDataset_converted.pkl', 'wb'))
